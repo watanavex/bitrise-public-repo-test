80cec436edcadfabd9b4d9fa08e81bec313d4bc7
watanave <susan.se3p@gmail.com>
2020-08-13 10:41:59 +0900

    Add xcode project

---
+++ BitrisePublicRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitrisePublicRepo
+//
+//  Created by Yohta Watanave on 2020/08/13.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
